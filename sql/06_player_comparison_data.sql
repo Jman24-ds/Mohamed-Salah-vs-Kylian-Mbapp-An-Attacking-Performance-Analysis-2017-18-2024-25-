39ca2655c1533d6846f8c4d56d64997e5d4350f7
Jman24-ds <joedelmangu@gmail.com>
2026-07-20 18:33:58 -0500

    Create 06_player_comparison_data.sql

---
+++ sql/06_player_comparison_data.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM `football_1718_2425.1718-2425_football`
+WHERE long_name IN (
+  'Mohamed Salah Ghaly',
+  'Mohamed Salah Hamed Ghaly',
+  'Kylian Mbappe Lottin'
+)
+ORDER BY long_name, season;
